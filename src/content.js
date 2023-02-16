47e3b70ec1c4d12904e2392c9d75ae0c2e75d53b
Pohakoo <kaisereni@cascadesacademy.org>
2023-02-15 19:35:02 -0800

    update

--- src/content.js
+++ src/content.js
@@ -43,25 +43,82 @@ function file(name) {
     return(chrome.runtime.getURL('src/resources/' + name));
 }
 
-function assignmentCenter(i) {
-    //Defines a bunch of nodes to change
-    const body = document.getElementsByTagName("body")[0];
-    const headerContainer = document.getElementById("site-header-container");
-    //const logoImage = document.getElementById("site-logo").getElementsByTagName("img")[0];
-    const navContainer = document.getElementById("site-nav-container");
+function allPages() {
+    const logoImage = document.getElementById("site-logo").getElementsByTagName("img")[0];
     const chalkboardImage = document.getElementById("group-header-Classes").getElementsByTagName("img")[0];
     const groupsImage = document.getElementById("group-header-Groups").getElementsByTagName("img")[0];
     const resourcesImage = document.getElementById("group-header-Resources").getElementsByTagName("img")[0];
     const newsImage = document.getElementById("group-header-News").getElementsByTagName("img")[0];
     const calendarImage = document.getElementById("calendar-subnav").getElementsByTagName("img")[0];
     const directoryImage = document.getElementById("directory-subnav").getElementsByTagName("img")[0];
+    const headerContainer = document.getElementById("site-header-container");
+    const navContainer = document.getElementById("site-nav-container");
     const navOptions = navContainer.getElementsByClassName("title sky-nav");
     const navSubs = navContainer.getElementsByClassName("desc");
     const navDropArrows = navContainer.getElementsByClassName("caret");
     const subNavBar = document.getElementById("site-nav-lower");
+    
+    //Change some images/icons on the site to my own. These images can be found in src/resources/
+    chalkboardImage.src = file("chalkboard-icon.png");
+    groupsImage.src = file("groups-icon.png");
+    resourcesImage.src = file("books-icon.png");
+    newsImage.src = file("newspaper-icon.png");
+    calendarImage.src = file("calendar-icon.png");
+    directoryImage.src = file("directory-icon.png");
+    logoImage.src = file("kaiscades-academy.png");
+
+    //The original code is awful. Idk why I need to do this but I do
+    headerContainer.classList.remove('pri-100-bgc');
+    navContainer.classList.remove('sec-15-bgc');
+
+    //Some bulk edits
+    bulkedit(navOptions, { "color": "white" }, "elements", "pri-100-fgc");
+    bulkedit(navSubs, { "color": "white" }, "elements", "black-fgc");
+    bulkedit(navDropArrows, { "color": "white" }, "elements", null);
+    
+    css(headerContainer, {
+        "background-color": 'rgb(30 45 89)',
+    });
+    css(navContainer, {
+        "background-color": "#293b72",
+        "font-family": "raleway",
+    });
+    css(subNavBar, {
+        "color": "white"
+    });
+    css(chalkboardImage, {
+        "transform": "translateY(2px)",
+        "width": "30px",
+        "height": "25px"
+    })
+    css(groupsImage, {
+        "width": "28px",
+        "height": "28px"
+    })
+    css(resourcesImage, {
+        "width": "30px",
+        "height": "30px"
+    })
+    css(newsImage, {
+        "width": "29px",
+        "height": "29px"
+    })
+    css(calendarImage, {
+        "width": "28px",
+        "height": "28px"
+    })
+    css(directoryImage, {
+        "width": "28px",
+        "height": "28px"
+    })
+    
+}
+
+function assignmentCenter() {
+    //Defines a bunch of nodes to change
+    const body = document.getElementsByTagName("body")[0];
     //These lists of nodes will all be edited at once with the bulkEdit() function
     const subNavButtonNames = ["checklist-btn", "requirement-btn", "course-requests-btn", "assignment-center-btn", "schedule-btn", "progress-btn"]
-    const linkButtonNames = ["bulletin-board-link", "topics-link", "assignments-link", "roster-link"];
     const dateDisplay = document.getElementById("small-date-display-label");
     const assignmentFiltersHeader = document.getElementsByClassName("bb-action-bar hidden-xs hidden-sm")[0];
     const assignmentFilters = document.getElementsByClassName("bb-action-bar hidden-xs hidden-sm")[1];
@@ -79,27 +136,17 @@ function assignmentCenter(i) {
     const assignmentsBackground = document.getElementsByClassName("bb-tile-content")[0];
     const assignmentsHeader = document.getElementsByClassName("bb-tile-header")[0];
 
-    //The original code is awful. Idk why I need to do this but I do
-    headerContainer.classList.remove('pri-100-bgc');
-    navContainer.classList.remove('sec-15-bgc');
-
     try {
         dateDisplay.remove();
     }
     catch(error){}
-    if (i==1) {
-        try {
-            uselessButtons1.remove();
-        }
-        catch(error){}
+    try {
+        uselessButtons1.remove();
     }
+    catch(error){}
 
     //Bulk edit some lists of elements
-    bulkedit(navOptions, { "color": "white" }, "elements", "pri-100-fgc");
-    bulkedit(navSubs, { "color": "white" }, "elements", "black-fgc");
-    bulkedit(navDropArrows, { "color": "white" }, "elements", null);
     bulkedit(subNavButtonNames, { "color": "black" }, "id", null);
-    bulkedit(linkButtonNames, { "color": "black" }, "id", null);
     bulkedit(assignmentItems, { "background-color": "#414a72" }, "elements", null);
     bulkedit(assignmentSortText, { "color": "white"}, "elements", null);
     bulkedit(assignmentFilterButtons1, { "color": "white", "background-color": "#262935"}, "elements", null);
@@ -145,55 +192,11 @@ function assignmentCenter(i) {
 
     }
 
-    //Change some images/icons on the site to my own. These images can be found in src/resources/
-    chalkboardImage.src = file("chalkboard-icon.png");
-    groupsImage.src = file("groups-icon.png");
-    resourcesImage.src = file("books-icon.png");
-    newsImage.src = file("newspaper-icon.png");
-    calendarImage.src = file("calendar-icon.png");
-    directoryImage.src = file("directory-icon.png");
-    //logoImage.src = file("kaiscades-academy.png");
-
     //Edit the css of a bunch of elements using the css() function
-    css(chalkboardImage, {
-        "transform": "translateY(2px)",
-        "width": "30px",
-        "height": "25px"
-    })
-    css(groupsImage, {
-        "width": "28px",
-        "height": "28px"
-    })
-    css(resourcesImage, {
-        "width": "30px",
-        "height": "30px"
-    })
-    css(newsImage, {
-        "width": "29px",
-        "height": "29px"
-    })
-    css(calendarImage, {
-        "width": "28px",
-        "height": "28px"
-    })
-    css(directoryImage, {
-        "width": "28px",
-        "height": "28px"
-    })
-
     css(body, {
         "background-color": '#2b2b3a'
     });
-    css(headerContainer, {
-        "background-color": 'rgb(30 45 89)',
-    });
-    css(navContainer, {
-        "background-color": "#293b72",
-        "font-family": "raleway",
-    });
-    css(subNavBar, {
-        "color": "white"
-    });
+    
     css(assignmentFiltersHeader, {
         "background-color": "#293b72",
         "border-top": "none",
@@ -242,6 +245,56 @@ function assignmentCenter(i) {
 
 }
 
+function assignmentDetail() {
+    const body = document.getElementsByTagName("body")[0];
+    const titleHeaderText = document.getElementsByClassName("bb-tile-header")[0];
+    const statusText = document.getElementsByClassName("bb-tile-header")[1];
+    const titleHeader = document.getElementsByClassName("bb-tile-title")[0];
+    const statusHeader = document.getElementsByClassName("bb-tile-title")[1];
+    const viewPanels = document.getElementsByClassName("bb-tile-content");
+    const statusDropdown = document.getElementsByClassName("tile-body whiteContainer1")[0];
+    const statusBackground = document.getElementsByClassName("assignment-detail-header completed")[0];
+    const statusIndicator = document.getElementsByClassName("indicator-field p3formWhite dropdown-toggle assignment-status-button")[0];
+
+    bulkedit(viewPanels, { 'border-radius': '0px 0px 25px 25px' }, "elements", null);
+
+    css(body, {
+        'background-color': "#2b2b3a"
+    })
+    css (titleHeaderText, {
+        "font-family": "nunito",
+        "color": "white",
+        "text-shadow": "0px 0px 10px black",
+        'transform': 'translate(60%, 2px)'
+    });
+    css (statusText, {
+        "font-family": "nunito",
+        "color": "white",
+        "text-shadow": "0px 0px 10px black",
+        'transform': 'translateY(3px)'
+    });
+    css (titleHeader, {
+        "border": "2px solid white",
+        'border-radius': '25px 25px 0px 0px',
+        "background-color": 'rgb(30 45 89)',
+    });
+    css (statusHeader, {
+        "border": "2px solid white",
+        'border-radius': '25px 25px 0px 0px',
+        "background-color": 'rgb(30 45 89)',
+    });
+    css (statusDropdown, {
+        "border-radius": "10px"
+    })
+    css (statusBackground, {
+        "background-color": "transparent"
+    })
+    css (statusIndicator, {
+        "border-radius": "9px",
+        "width": "110px"
+    })
+}
+
 //Init is called when the window loads.
 function init() {
     //adds some new fonts to the page's font library
@@ -252,25 +305,32 @@ function init() {
     document.fonts.add(ralewayThin);
     document.fonts.add(nunito);
     //does a different list of actions depending on the page within steelhead
-    var i = 1;
+    var i = 0;
     let loc = window.location.href;
     function loop() {
+        allPages();
         if (!(loc == window.location.href)) {
             loc = window.location.href;
             i = 0;
         }
-        if (window.location.href.indexOf("studentmyday/assignment-center") > -1) {
-            try {
-                assignmentCenter(i);
+        if (i == 0) {
+            if (window.location.href.indexOf("studentmyday/assignment-center") > -1) {
+                try {
+                    assignmentCenter();
+                }
+                catch(error){}
             }
-            catch(error){}
-        }
-        else if (window.location.href.indexOf("assignmentdetail") > -1) {
-            console.log("detail");
+            else if (window.location.href.indexOf("assignmentdetail") > -1) {
+                assignmentDetail();
+            }
         }
         i++;
-
-        setTimeout(() => {loop();}, 1500);
+        if (i<3) {
+            setTimeout(() => {loop();}, 300);
+        }
+        else {
+            setTimeout(() => {loop();}, 800);
+        }
     }
     loop();
 
